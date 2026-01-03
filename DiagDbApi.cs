51e59240b5026e3a410c46a90f9165c78a76c959
Cashier User <you@example.com>
2026-01-03 03:57:19 +0300

    fix: db diag uses relational GetDbConnection

--- DiagDbApi.cs
+++ DiagDbApi.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using NaderProductsApp.Data;
 
 public static class DiagDbApi
@@ -8,8 +11,10 @@ public static class DiagDbApi
         app.MapGet("/api/_diag/db", ([FromServices] AppDbContext db) =>
         {
             var provider = db.Database.ProviderName ?? "";
-            var conn = db.Database.GetDbConnection();
-            return Results.Ok(new {
+            var conn = db.Database.GetDbConnection(); // يحتاج Relational extensions
+
+            return Results.Ok(new
+            {
                 provider,
                 dataSource = conn.DataSource,
                 database = conn.Database
